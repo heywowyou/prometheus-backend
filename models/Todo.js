b76db0cc41b01111819d4a1850426636d8f318ec
heywowyou <pro@julesfontaine.com>
2025-12-12 15:04:49 +0100

    Update models/Todo.js with interaction type and duration goal

--- models/Todo.js
+++ models/Todo.js
@@ -25,6 +25,18 @@ const TodoSchema = mongoose.Schema(
     },
     // Total number of times this task was completed
     completionCount: { type: Number, default: 0 },
+    // Tactile habits
+    interactionType: {
+      type: String,
+      // 'checkbox' = standard click
+      // 'hold' = long press to fill (3 seconds)
+      enum: ["checkbox", "hold"],
+      default: "checkbox",
+    },
+    durationGoal: {
+      type: Number, // The label to display (e.g. 10 for "10 mins")
+      default: 0,
+    },
     // The unique ID of the user who owns this task (from Clerk)
     userId: {
       type: String,
